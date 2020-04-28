6b57a9252e9b327431aeb532a65320111f94915b
Leo Carrico <lcarrico827@gmail.com>
2020-04-28 18:46:12 -0500

    added scripts

--- code/deltas.py
+++ code/deltas.py
@@ -15,7 +15,7 @@ def main():
     # sampler.run()
     # features.run()
 
-    model = models.RandomForest
+    model = models.RandomForest()
 
     X_train, X_test, y_train, y_test = engineer.run(model = model)
 
@@ -28,6 +28,23 @@ def main():
 # main()
 
 def example():
+
+    # generate samples
+    delta_file = "/home/shared/CMV/SortedData/delta_comments_data.csv"
+    nodelta_file = "/home/shared/CMV/SortedData/nodelta_comments_data.csv"
+
+    delta_sample_file = "/home/shared/CMV/SampledData/delta_sample_data.csv"
+    nodelta_sample_file = "/home/shared/CMV/SampledData/nodelta_sample_data.csv"
+
+    print("Sampling Delta File")
+    sampler.sample(delta_file, delta_sample_file, 13000)
+
+    print("Sampling NoDelta File")
+    sampler.sample(nodelta_file, nodelta_sample_file, 13000)
+
+    #generate features
+    features.run()
+
     # load your data
     nodelta_data = pd.read_csv("/home/shared/CMV/FeatureData/nodelta_sample_feature_data.csv")
     delta_data = pd.read_csv("/home/shared/CMV/FeatureData/delta_sample_feature_data.csv")
@@ -38,23 +55,30 @@ def example():
     # separate features and labels
     X, y = data[: , :-1], data[:, -1]
 
-    # creates new features where its squared
-    X = engineer.squareFeatures(X)
-
-    # split into train and test
     X_train, X_test, y_train, y_test = engineer.train_test_split(X, y, test_size=0.33)
-
-    # pick a model
-    model = models.GradientBoosting
-
-    # train the model
+    model = models.LogisticRegressionClassifier(c=1)
     model = model.fit(X_train, y_train)
-
-    # predict with the model
     y_pred = model.predict(X_test)
-
-    # score the model
     score = accuracy.score(y_pred, y_test)
     print(score)
 
-example()
+
+    # creates new features where its squared
+    X_2 = engineer.squareFeatures(X)
+    X_2_train, X_2_test, y_2_train, y_2_test = engineer.train_test_split(X_2, y, test_size=0.33)
+
+
+    # pick a model
+    model_2 = models.LogisticRegressionClassifier(c=1)
+
+    # train the model
+    model_2 = model_2.fit(X_2_train, y_2_train)
+
+    # predict with the model
+    y_2_pred = model_2.predict(X_2_test)
+
+    # score the model
+    score = accuracy.score(y_2_pred, y_2_test)
+    print(score)
+
+# example()
--- code/models.py
+++ code/models.py
@@ -6,35 +6,58 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model import Perceptron
 from sklearn.neural_network import MLPClassifier
 from sklearn.tree import DecisionTreeClassifier
+from keras.models import Sequential
+from keras.layers import Dense
 
-names = ["AdaBoost", "GradientBoosting", "Perceptron",
-    "LogisticRegression", "RandomForest", "MLP", "DecisionTree",
+names = ["AdaBoost", "GradientBoosting", "PerceptronClassifier",
+    "LogisticRegressionClassifier", "RandomForest", "MLP", "DecisionTree",
     "MultinomialNB"]
 
-AdaBoost = AdaBoostClassifier(n_estimators=100, random_state=0)
+def AdaBoost(): return AdaBoostClassifier(n_estimators=100, random_state=0)
 
-GradientBoosting = GradientBoostingClassifier(n_estimators=100, random_state=0)
+def GradientBoosting(): return GradientBoostingClassifier(n_estimators=100, random_state=0)
 
-Perceptron = Perceptron(tol=1e-6)
+def PerceptronClassifier(): return Perceptron(tol=1e-6)
 
-LogisticRegression = LogisticRegression(max_iter=10000, solver="liblinear", penalty="l1",
-            class_weight="balanced", C=1e-1)
+def LogisticRegressionClassifier(c=1):
+    return LogisticRegression(C=c)
 
-RandomForest = RandomForestClassifier(bootstrap=True, class_weight=None, criterion='entropy',
+def RandomForest(): return RandomForestClassifier(bootstrap=True, class_weight=None, criterion='entropy',
             max_depth=80, max_leaf_nodes=None,
             min_impurity_decrease=0.0, min_impurity_split=None,
             min_samples_leaf=3, min_samples_split=12,
             min_weight_fraction_leaf=0.0, n_estimators=1000, n_jobs=None,
             oob_score=False, random_state=0, verbose=0, warm_start=False)
 
-MLP = MLPClassifier(solver='lbfgs', alpha=1e-5,
+def MLP(): return MLPClassifier(solver='lbfgs', alpha=1e-5,
             hidden_layer_sizes=(10, 50), random_state=1)
 
-DecisionTree = DecisionTreeClassifier(class_weight=None, criterion='entropy', max_depth=None,
+def DecisionTree(): return DecisionTreeClassifier(class_weight=None, criterion='entropy', max_depth=None,
             max_features=None, max_leaf_nodes=None,
             min_impurity_decrease=0.0, min_impurity_split=None,
             min_samples_leaf=1, min_samples_split=2,
             min_weight_fraction_leaf=0.0, presort=False, random_state=None,
             splitter='best')
 
-MultinomialNB = MultinomialNB(alpha=1.0, class_prior=None, fit_prior=True)
+def MultinomialNB(): return MultinomialNB(alpha=1.0, class_prior=None, fit_prior=True)
+
+
+class FeedForwardNeuralNetwork:
+    def __init__(self, inputs=10, outputs=1, layers=[10,10], epochs=50):
+        self.model = Sequential()
+        for i in range(len(layers)):
+            if i == 0:
+                self.model.add(Dense(layers[i], input_dim=inputs, activation='relu'))
+            else:
+                self.model.add(Dense(layers[i], activation='relu'))
+        self.model.add(Dense(outputs, activation='sigmoid'))
+        self.model.compile(loss='binary_crossentropy', optimizer='adam')
+        self.epochs = epochs
+
+    def fit(self, X, y):
+        self.model.fit(X, y, epochs=self.epochs, verbose=1)
+        return self
+
+    def predict(self, X):
+        return self.model.predict_classes(X)
+        return self
