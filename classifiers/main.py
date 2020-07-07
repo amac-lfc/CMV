f443cdf7eb99e537f72b985fe3ce7d580bc78c93
Arthur Bousquet <bousquet@mx.lakeforest.edu>
2020-07-06 21:35:41 -0500

    v1

--- classifiers/main.py
+++ classifiers/main.py
@@ -87,14 +87,12 @@ if __name__ == '__main__':
     2 : "AdaBoost"
     3 : "GradientBoosting"
     4 : "LogisticRegression"
-    5 : "MLP"
-    6 : "DecisionTree"
-    7 : 'GaussianNB'
-    8 : 'BernoulliNB' 
-    9 : 'SVM'
-    10: 'SGD'
+    5 : "DecisionTree"
+    6 : 'GaussianNB'
+    7 : 'BernoulliNB' 
+    8 : 'SVM'
     '''
-    ModelList= [1,2,3,4,6,7,8,9]
+    ModelList= [1,2,3,4,6,7,8]
     # ModelList=[1]
 
     print("Prepping Data")
--- classifiers/models.py
+++ classifiers/models.py
@@ -2,19 +2,17 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.linear_model import LogisticRegression as LogisticRegressionClassifier
-from sklearn.neural_network import MLPClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.naive_bayes import BernoulliNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
-from sklearn.linear_model import SGDClassifier
 import numpy as np
 
 import sys
 sys.setrecursionlimit(10000)
 
-names = np.array(["RandomForest", "AdaBoost", "GradientBoosting","LogisticRegression", "MLP", "DecisionTree", 'GaussianNB', 'BernoulliNB' ,'SVM', 'SGD'])
+names = np.array(["RandomForest", "AdaBoost", "GradientBoosting","LogisticRegression", "DecisionTree", 'GaussianNB', 'BernoulliNB' ,'SVM'])
 
 def AdaBoost(): return AdaBoostClassifier(n_estimators=100, random_state=0)
 
@@ -30,9 +28,6 @@ def RandomForest(): return RandomForestClassifier(bootstrap=True, class_weight=N
             min_weight_fraction_leaf=0.0, n_estimators=1000, n_jobs=None,
             random_state=0, verbose=0, warm_start=True, oob_score=True)
 
-def MLP(): return MLPClassifier(solver='lbfgs', alpha=1e-5,
-            hidden_layer_sizes=(10, 50), random_state=1)
-
 def DecisionTree(): return DecisionTreeClassifier(class_weight=None, criterion='entropy', max_depth=None,
             max_features=None, max_leaf_nodes=None,
             min_impurity_decrease=0.0, min_impurity_split=None,
@@ -41,5 +36,3 @@ def DecisionTree(): return DecisionTreeClassifier(class_weight=None, criterion='
             splitter='best')
 
 def SVM(C=1.0) : return SVC(C=C)
-
-def SGD(): return SGDClassifier(max_iter=100000,penalty='l2')
